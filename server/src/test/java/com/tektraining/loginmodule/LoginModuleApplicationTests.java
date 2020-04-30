2615243a5d080d91c077c18cae5dcbe790a750aa
madhur-taneja <tanejamadhur96@gmail.com>
2020-04-30 09:03:04 +0530

    BE: Commenting Test import

--- server/src/test/java/com/tektraining/loginmodule/LoginModuleApplicationTests.java
+++ server/src/test/java/com/tektraining/loginmodule/LoginModuleApplicationTests.java
@@ -1,6 +1,6 @@
 package com.tektraining.loginmodule;
 
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
