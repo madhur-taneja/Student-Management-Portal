24ba4ebe1d6d9c6f447ddc3680f1221e56079b1c
madhur-taneja <tanejamadhur96@gmail.com>
2020-05-18 15:26:46 +0530

    FE: Updating Home * Page Layout * Adding login button

--- client/src/components/home.component.tsx
+++ client/src/components/home.component.tsx
@@ -1,13 +1,31 @@
 import React, { Component } from "react";
 
 import UserService from "../services/user.service";
+import { Link } from "react-router-dom";
+
+import AuthService from "../services/auth.service";
+
+const spanStyling = {
+  padding: '10px',
+  display: 'inline',
+  border: '0',
+  background: 'blue',
+  color: 'white',
+  width: 'auto',
+  height: 'auto',
+  fontSize: '15px',
+  lineHeight: '15px',
+  fontFamily: 'Arial',
+  borderRadius: '2px'
+};
 
 export default class Home extends Component<any, any> {
   constructor(props: any) {
     super(props);
 
     this.state = {
-      content: ""
+      content: "",
+      currentUser: AuthService.getCurrentUser()
     };
   }
 
@@ -33,9 +51,18 @@ export default class Home extends Component<any, any> {
     return (
       <div className="container">
         <header className="jumbotron">
-          <h3>{this.state.content}</h3>
+          <h1>Student Management Portal</h1>
+          <p>Welcome! This portal is for students to access their grades online at the click of a button</p>
+          <div className="my-5">
+            <h3>{this.state.content}</h3>
+          </div>
+          {!this.state.currentUser && (
+            <span className="input-group-btn" style={spanStyling}>
+              <Link style={{ "color": "white", font: "bold" }} to="/login">Go to Login Page</Link>
+            </span>
+          )}
         </header>
-      </div>
+      </div >
     );
   }
 }
